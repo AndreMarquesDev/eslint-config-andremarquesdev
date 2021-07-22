bee820ab324ac98181eb0ae3961a258c1b8c0162
AndreMarquesDev <andregm_7@hotmail.com>
2021-07-23 00:15:08 +0100

    fix: Fix typescript rules not being properly applied

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,152 +8,86 @@ module.exports = {
     extends: [
         'airbnb',
         'airbnb/hooks',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
         'plugin:jsx-a11y/recommended',
         'plugin:prettier/recommended',
     ],
-    plugins: [
-        'babel',
-        '@typescript-eslint/eslint-plugin',
-        'react-hooks',
-        'jsx-a11y',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        sourceType: 'module',
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-            jsx: true,
-        },
-        ecmaVersion: 2019
-    },
+    plugins: ['babel', 'react-hooks', 'jsx-a11y'],
     rules: {
         //
-        // General
+        // general
         //
 
         // enforces a minimum identifier length convention
         'id-length': [
             'error',
             {
-                min: 4,
-                exceptions: [
-                    'App',
-                    'app',
-                    'Row',
-                    'row',
-                    'Id',
-                    'id',
-                    'css',
-                    'Css',
-                    'env',
-                    'Env',
-                    'url',
-                    'URL',
-                    'top',
-                    'Top',
-                    'req',
-                    'res',
-                    'key',
-                    'Key',
-                    'all',
-                    'All',
-                    'of',
-                    'Of',
-                    'src',
-                    'Src',
-                    'alt',
-                    'Alt',
-                    'cta',
-                    'Cta',
-                    'add',
-                    'Add',
-                    'min',
-                    'Min',
-                    'max',
-                    'Max',
-                ]
-            }
+                min: 3,
+                exceptions: ['Id', 'id', 'of', 'Of'],
+            },
         ],
         // disallows the renaming of import, export and destructured assignments to the same name
         'no-useless-rename': 'error',
         'max-len': 'off',
         'linebreak-style': 'off',
-        'import/extensions': [
-            2,
-            'never',
-        ],
+        'import/extensions': [2, 'never'],
         'import/no-extraneous-dependencies': [
             'error',
             {
-                'devDependencies': [
+                devDependencies: [
                     '**/*.test.ts',
                     '**/*.test.tsx',
                     '**/tests/*.js',
                     '**/*.dev.js',
                     '**/*.dev.ts',
-                ]
-            }
+                ],
+            },
         ],
         'import/no-unresolved': [
             2,
             {
-                ignore: ['\\.(png|jpg|svg)\\?(lqip)$']
-            }
+                ignore: ['\\.(png|jpg|svg)\\?(lqip)$'],
+            },
         ],
         'import/prefer-default-export': 'off',
         radix: 'off',
         'no-plusplus': 'off',
 
-
         //
-        // Functions
+        // functions
         //
 
-        'arrow-parens': [
-            'error',
-            'as-needed'
-        ],
+        'arrow-parens': ['error', 'as-needed'],
         // requires all immediately-invoked function expressions to be wrapped in parenthesis
-        'wrap-iife': [
-            'error',
-            'inside',
-        ],
-        'function-paren-newline': [
-            'error',
-            'never',
-        ],
+        'wrap-iife': ['error', 'inside'],
 
         //
-        // Spacings
+        // spacings
         //
 
         'object-curly-newline': [
             'error',
             {
                 ObjectExpression: {
-                    minProperties: 1
+                    minProperties: 1,
                 },
                 ImportDeclaration: 'never',
                 ExportDeclaration: {
                     multiline: true,
                     minProperties: 3,
-                }
-            }
+                },
+            },
         ],
         'object-property-newline': [
             'error',
             {
-                'allowAllPropertiesOnSameLine': false
-            }
+                allowAllPropertiesOnSameLine: false,
+            },
         ],
         indent: [
             'error',
             4,
             {
-                'SwitchCase': 1
+                SwitchCase: 1,
             },
         ],
         // spacing before and after semicolons
@@ -161,16 +95,16 @@ module.exports = {
             'error',
             {
                 before: false,
-                after: true
-            }
+                after: true,
+            },
         ],
         // spacing before and after colons in switch statements
         'switch-colon-spacing': [
             'error',
             {
                 after: true,
-                before: false
-            }
+                before: false,
+            },
         ],
         'rest-spread-spacing': ['error'],
         'padding-line-between-statements': [
@@ -178,130 +112,89 @@ module.exports = {
             {
                 blankLine: 'always',
                 prev: '*',
-                next: 'return'
+                next: 'return',
             },
             {
                 blankLine: 'always',
                 prev: ['const', 'let', 'var'],
-                next: '*'
+                next: '*',
             },
             {
                 blankLine: 'any',
                 prev: ['const', 'let', 'var'],
-                next: ['const', 'let', 'var']
+                next: ['const', 'let', 'var'],
             },
         ],
 
-
         //
-        // Comments
+        // comments
         //
 
         'capitalized-comments': [
             'error',
             'never',
             {
-                'ignorePattern': 'TODO:',
-            }
+                ignorePattern: 'TODO:',
+            },
         ],
         'line-comment-position': [
             'error',
             {
-                position: 'above'
-            }
+                position: 'above',
+            },
         ],
-        'multiline-comment-style': [
-            'warn',
-            'separate-lines'
-        ],
-
+        'multiline-comment-style': ['warn', 'separate-lines'],
 
         //
-        // React
+        // react
         //
 
         'react/jsx-no-bind': [
             2,
             {
                 allowArrowFunctions: true,
-            }
+            },
         ],
-        // Since we already use typescript , this just does not give any warning if prop-types are not even declared
+        // since we already use typescript , this just does not give any warning if prop-types are not even declared
         'react/prop-types': [
             2,
             {
                 skipUndeclared: true,
-            }
+            },
         ],
         'react/jsx-filename-extension': [
             1,
             {
-                extensions: [
-                    '.js',
-                    '.jsx',
-                    '.tsx',
-                    '.ts',
-                ]
-            }
+                extensions: ['.js', '.jsx', '.tsx', '.ts'],
+            },
         ],
-        'react/jsx-indent': [
-            'error',
-            4
-        ],
-        'react/jsx-indent-props': [
-            'error',
-            4
-        ],
-        'react/jsx-first-prop-new-line': [
-            1,
-            'multiline-multiprop',
-        ],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-first-prop-new-line': [1, 'multiline-multiprop'],
         'react/jsx-sort-props': [
             1,
             {
                 callbacksLast: true,
                 shorthandFirst: true,
                 reservedFirst: true,
-            }
+            },
         ],
         'react/jsx-props-no-spreading': 'error',
         'react/jsx-curly-spacing': [
             2,
             {
-                'when': 'never',
-                'children': true
-            }
+                when: 'never',
+                children: true,
+            },
         ],
 
-
-        //
         //
         // a11y
+        //
         'jsx-a11y/label-has-associated-control': 'off',
 
-
         //
-        // Typescript
-        //
-        '@typescript-eslint/no-explicit-any': 'error',
-        '@typescript-eslint/indent': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/ban-ts-ignore': 'off',
-        '@typescript-eslint/naming-convention': [
-            'error',
-            {
-                'selector': 'interface',
-                'format': ['PascalCase'],
-                'custom': {
-                    'regex': '^I[A-Z]',
-                    'match': false
-                }
-            }
-        ],
-
-
-        //
-        // Prettier config
+        // prettier config
         //
         'prettier/prettier': [
             'error',
@@ -317,31 +210,56 @@ module.exports = {
                 endOfLine: 'auto',
             },
         ],
-
-
-        // check overrides below
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
     },
-    overrides: [{
-        // specific rules for TypeScript files
-        files: [
-            '*.ts',
-            '*.tsx',
-        ],
-        rules: {
-            '@typescript-eslint/explicit-function-return-type': 'error',
-            '@typescript-eslint/no-var-requires': 'error',
-        }
-    }, {
-        // specific rules for test files
-        files: [
-            '*test.js',
-            '*test.ts',
-            '*test.tsx',
-        ],
-        rules: {
-            'react/jsx-props-no-spreading': 'off'
-        }
-    }],
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+    overrides: [
+        {
+            // specific rules for TypeScript files
+            files: ['**/*.ts', '**/*.tsx'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+            extends: [
+                'plugin:@typescript-eslint/eslint-recommended',
+                'plugin:@typescript-eslint/recommended',
+                'plugin:import/typescript',
+            ],
+            plugins: ['@typescript-eslint'],
+            rules: {
+                '@typescript-eslint/no-explicit-any': 'error',
+                '@typescript-eslint/naming-convention': [
+                    'error',
+                    {
+                        selector: 'interface',
+                        format: ['PascalCase'],
+                        custom: {
+                            regex: '^I[A-Z,a-z]',
+                            match: false,
+                        },
+                    },
+                ],
+                '@typescript-eslint/explicit-function-return-type': 'error',
+                '@typescript-eslint/no-var-requires': 'error',
+                'no-use-before-define': 'off',
+                '@typescript-eslint/no-use-before-define': ['error'],
+                'no-shadow': 'off',
+                '@typescript-eslint/no-shadow': ['error'],
+                '@typescript-eslint/indent': 'off',
+                '@typescript-eslint/explicit-module-boundary-types': 'off',
+                '@typescript-eslint/ban-ts-ignore': 'off',
+            },
+        },
+        {
+            // specific rules for test files
+            files: ['*test.js', '*test.ts', '*test.tsx'],
+            rules: {
+                'react/jsx-props-no-spreading': 'off',
+            },
+        },
+    ],
 };
